ea58028f3b13fb7670617de107b40389a365ebf0
Lukas Rusak <lorusak@gmail.com>
2015-09-10 10:31:34 -0700

    [six] create package.mk

---
+++ packages/other/python/six/package.mk
@@ -0,0 +1,54 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="six"
+PKG_VERSION="1.9.0"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/DiffSK/configobj"
+PKG_URL="https://pypi.python.org/packages/source/s/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
+PKG_PRIORITY="optional"
+PKG_SECTION="python"
+PKG_SHORTDESC="Six is a Python 2 and 3 compatibility library."
+PKG_LONGDESC="Six is a Python 2 and 3 compatibility library."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_host() {
+ : # nothing todo
+}
+
+makeinstall_host() {
+  python setup.py install --prefix=$ROOT/$TOOLCHAIN
+}
+
+pre_make_target() {
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+  export LDSHARED="$CC -shared"
+}
+
+make_target() {
+  python setup.py build
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+}
