f7e63644e01e212d23e4cdae9f4d93dafc3be283
Lukas Rusak <lorusak@gmail.com>
2017-01-16 13:22:06 -0800

    connman: add support for gadget devices

--- packages/network/connman/package.mk
+++ packages/network/connman/package.mk
@@ -50,7 +50,7 @@ PKG_CONFIGURE_OPTS_TARGET="WPASUPPLICANT=/usr/bin/wpa_supplicant \
                            --disable-selinux \
                            --enable-loopback \
                            --enable-ethernet \
-                           --disable-gadget \
+                           --enable-gadget \
                            --enable-wifi \
                            --disable-bluetooth \
                            --disable-ofono \
@@ -91,7 +91,8 @@ post_makeinstall_target() {
         -e "s|^# BackgroundScanning.*|BackgroundScanning = true|g" \
         -e "s|^# FallbackNameservers.*|FallbackNameservers = 8.8.8.8,8.8.4.4|g" \
         -e "s|^# FallbackTimeservers.*|FallbackTimeservers = 0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org,3.pool.ntp.org|g" \
-        -e "s|^# PreferredTechnologies.*|PreferredTechnologies = ethernet,wifi,cellular|g" \
+        -e "s|^# DefaultAutoConnectTechnologies.*|DefaultAutoConnectTechnologies = ethernet,wifi,cellular,gadget|g" \
+        -e "s|^# PreferredTechnologies.*|PreferredTechnologies = ethernet,wifi,cellular,gadget|g" \
         -e "s|^# TetheringTechnologies.*|TetheringTechnologies = wifi|g" \
         -e "s|^# AllowHostnameUpdates.*|AllowHostnameUpdates = false|g" \
         -e "s|^# PersistentTetheringMode.*|PersistentTetheringMode = true|g" \
