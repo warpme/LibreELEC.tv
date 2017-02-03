2083240db2b58360a8a5c47cf93f60eb2a7d9ee1
Eric Schultz <eric@startuperic.com>
2017-02-03 02:33:24 -0800

    spice-protocol: add package

---
+++ packages/x11/proto/spice-protocol/package.mk
@@ -0,0 +1,31 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="spice-protocol"
+PKG_VERSION="0.12.12"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.spice-space.org/"
+PKG_URL="http://www.spice-space.org/download/releases/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="x11/proto"
+PKG_SHORTDESC="spice-protocol: SPICE headers"
+PKG_LONGDESC="SPICE headers"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
