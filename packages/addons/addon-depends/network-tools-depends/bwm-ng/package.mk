bc096180af888b05f61930de7bcc15ddf5c7a4af
Sascha Kuehndel (InuSasha) <dev@inusasha.de>
2017-05-08 00:54:15 +0200

    bwm-ng: gcc7 build patch switch to github source

--- packages/addons/addon-depends/network-tools-depends/bwm-ng/package.mk
+++ packages/addons/addon-depends/network-tools-depends/bwm-ng/package.mk
@@ -22,12 +22,12 @@ PKG_SHA256="027cf3c960cd96fc9ffacdf7713df62d0fc55eeef4a1388289f8a62ae5e50df0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gropp.org/?id=projects&sub=bwm-ng"
-PKG_URL="http://www.gropp.org/bwm-ng/bwm-ng-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/vgropp/bwm-ng/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain netbsd-curses libstatgrab"
 PKG_SECTION="network/analyzer"
 PKG_SHORTDESC="bwm-ng: small and simple console-based live network and disk io bandwidth monitor"
 PKG_LONGDESC="Bandwidth Monitor NG is a small and simple console-based live network and disk io bandwidth monitor for Linux, BSD, Solaris, Mac OS X and others."
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--with-libstatgrab \
                            --with-time \
