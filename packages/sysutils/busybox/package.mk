891c9045161b306ce6ab97bf497dca6c2ee11e31
Lukas Rusak <lorusak@gmail.com>
2017-09-11 20:19:21 -0700

    busybox: allow changing root password

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -159,7 +159,7 @@ makeinstall_target() {
     cp $PKG_DIR/scripts/createlog $INSTALL/usr/bin/
     cp $PKG_DIR/scripts/lsb_release $INSTALL/usr/bin/
     cp $PKG_DIR/scripts/apt-get $INSTALL/usr/bin/
-    cp $PKG_DIR/scripts/passwd $INSTALL/usr/bin/
+    #cp $PKG_DIR/scripts/passwd $INSTALL/usr/bin/
     cp $PKG_DIR/scripts/sudo $INSTALL/usr/bin/
     cp $PKG_DIR/scripts/pastebinit $INSTALL/usr/bin/
     ln -sf pastebinit $INSTALL/usr/bin/paste
