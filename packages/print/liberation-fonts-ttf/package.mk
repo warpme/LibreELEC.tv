19ec81198a3db01db7155bf2b51bff0eb2e851ab
Lukas Rusak <lorusak@gmail.com>
2018-12-05 18:06:56 -0800

    liberation-fonts-ttf: remove uneeded util-macros dependency

--- packages/print/liberation-fonts-ttf/package.mk
+++ packages/print/liberation-fonts-ttf/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="7890278a6cd17873c57d9cd785c2d230d9abdea837e96516019c5885dd271504"
 PKG_LICENSE="OFL1_1"
 PKG_SITE="https://www.redhat.com/promo/fonts/"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain util-macros"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="This packages included the high-quality and open-sourced TrueType vector fonts."
 PKG_TOOLCHAIN="manual"
 
